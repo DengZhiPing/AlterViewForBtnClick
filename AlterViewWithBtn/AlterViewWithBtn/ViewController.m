1fb36ad29bb67ed8c0d18b840434a9de4404fe07
dengzhiping <dengzhiping@dengzhipingdeMacBook-Pro.local>
2016-06-21 16:23:38 +0800

    提示

--- AlterViewWithBtn/AlterViewWithBtn/ViewController.m
+++ AlterViewWithBtn/AlterViewWithBtn/ViewController.m
@@ -23,7 +23,9 @@
     btn.backgroundColor = [UIColor greenColor];
     [btn addTarget:self action:@selector(btnPress) forControlEvents:UIControlEventTouchDown];
     btn.frame =CGRectMake(40, 40, 100, 100);
+    [btn setTitle:@"点我" forState:0];
     [self.view addSubview:btn];
+    self.view.backgroundColor = [UIColor grayColor];
 }
 
 - (void)btnPress {
